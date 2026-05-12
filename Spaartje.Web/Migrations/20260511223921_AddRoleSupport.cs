e29a0991fe15391efa1cd29f046dadc36a1692e8
YourName <111964150+Justindebuck@users.noreply.github.com>
2026-05-12 02:11:10 +0200

    Reapply "Test commit"
    This reverts commit 5764ece1aafa83a307cb5f1f936d91b53200b33b.

---
+++ Spaartje.Web/Migrations/20260511223921_AddRoleSupport.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Spaartje.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoleSupport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
