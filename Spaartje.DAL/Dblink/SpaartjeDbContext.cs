4d502713158e94e5b6bdac4b104f5e52b73d49db
Justin debuck <111964150+Justindebuck@users.noreply.github.com.>
2026-05-18 11:35:19 +0200

    Feat: Moved Data,Migrations folder to DAL
    Moved file Data and Migration to DAL and deleted redunded files in DAL

--- Spaartje.DAL/Dblink/SpaartjeDbContext.cs
+++
@@ -1,9 +0,0 @@
-
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-using Spaartje.Domain.Models;
-namespace Spaartje.DAL.Dblink;
-public class SpaartjeDbContext : IdentityDbContext<AppUser>
-{
-    public SpaartjeDbContext(DbContextOptions<SpaartjeDbContext> options) : base(options) { }
-}
