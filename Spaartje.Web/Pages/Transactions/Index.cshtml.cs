584f91c6192ee66e284c200c3932beacb17b954f
YourName <111964150+Justindebuck@users.noreply.github.com>
2026-06-07 23:15:35 +0200

    Bugfix: Fixed spacing issues

--- Spaartje.Web/Pages/Transactions/Index.cshtml.cs
+++ Spaartje.Web/Pages/Transactions/Index.cshtml.cs
@@ -54,7 +54,7 @@ public class IndexModel : PageModel
 
         [Required(ErrorMessage = "Amount is required")]
         [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than zero")]
-        public decimal Amount { get; set; }
+        public decimal? Amount { get; set; }
 
         [Required(ErrorMessage = "Date is required")]
         public DateTime Date { get; set; } = DateTime.Today;
@@ -85,7 +85,7 @@ public class IndexModel : PageModel
         try
         {
             await _transactionService.CreateTransactionAsync(
-                Input.Amount,
+                Input.Amount!.Value,
                 Input.Description,
                 Input.Date,
                 Input.Type,
